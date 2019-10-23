b4b4aadb5160b9dadb03368002b5862860d64e4f
markclynch <mark.christinat@gmail.com>
2019-10-23 04:16:46 -0700

    remove gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import styled, { css } from 'styled-components'
 import './App.css'
-import { isTSModuleBlock } from '@babel/types'
 type FormElem = React.FormEvent<HTMLFormElement>
 
 interface ITodo {
